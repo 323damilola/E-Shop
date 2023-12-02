c2e107d23785acc9d8b57be752d0fa39cc941af4
323damilola <damilolaogunbayo323@gmail.com>
2023-12-01 19:39:28 -0800

    Home Page Refixed

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jtMEHatjG1fniMSo3a2WtjEyO8EhlbRsZiTQVMf8Vgc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jGC+ncMsWlu/oM/QX3+J9gE/IlS3VOOF2IaIcbKUt9U=\"\n}"
